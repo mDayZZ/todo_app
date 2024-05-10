bfe3a2c8a0d3c4540e71995bf6861c5b9c359c9f
MDayZZ <maxdayZZOff@gmail.com>
2024-05-10 03:21:24 +0300

    + local Storage + delete/finish buttons functional - some tag list bugs + vibe - sleep time
    Thx <3

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const createTaskForm = document.querySelector('#createTaskForm')
 const titleInput = document.querySelector('#titleInput')
 const taskList = document.querySelector('#taskList')
 const listFilter = document.querySelector('#list_filter')
+const emptyCardElement = document.querySelector('.task-list-empty')
+
+isDev = false
+
+let currentOptionTag = 'all'
 
 function Task(id, title, tags, isFinished) {
     this.id = id
@@ -10,27 +15,76 @@ function Task(id, title, tags, isFinished) {
     this.isFinished = isFinished
 }
 
-const tasks = [
+let tasks = [
     {
         id: 1,
-        title: 'Buy cheeze',
+        title: 'Buy popcorn',
         tags: ['cart','for_movie'],
+        isFinished: true,
     },
     {
         id: 2,
-        title: 'Buy wine',
-        tags: ['cart','for_movie','date'],
+        title: 'add tags by #tag in input',
+        tags: ['hints','en'],
+        isFinished: false,
     },
     {
         id: 3,
-        title: 'Buy love <3',
-        tags: ['sex','for_movie','date', 'without_gf'],
+        title: 'Добавь теги, вписав #свой_тег в строке',
+        tags: ['hints','ru'],
+        isFinished: false,
+    },
+    {
+        id: 4,
+        title: 'Meet my love <3',
+        tags: ['love','sex', 'dinosaurs'],
+        isFinished: true,
     },
 ]
 
+if (localStorage.getItem('tasks') != null ) tasks = JSON.parse(localStorage.getItem('tasks'))
+
+saveTasks = function (tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 
 
+console.log(tasks)
 
+const getTaskIndexById = function (id) {
+    for (let taskIndex = 0; taskIndex < tasks.length; taskIndex++) {
+        isDev&&console.log('indexById log:\n',
+            '\ntaskIndex', taskIndex,
+            '\ntasks.length', tasks.length,
+            '\ntasks[taskIndex].id', tasks[taskIndex].id,
+            '\nid', id)
+        if (tasks[taskIndex].id === id) {
+            return taskIndex
+        }
+    }
+    return null
+}
+
+const getTaskById = function (id) {
+    for (let taskIndex = 0; taskIndex < tasks.length; taskIndex++) {
+        if (tasks[taskIndex].id === id) {
+            return tasks[taskIndex]
+        }
+    }
+    return null
+}
+
+// const emptyCardElement = (()=>{
+//     const cardElement = document.createElement('div')
+//     cardElement.classList.add('task-list-empty', 'pixel-corners')
+//     cardElement.innerHTML = `
+//                 <h3 class="visually-hidden">Task list is empty</h3>
+//                 <div class="task-list-empty-content">
+//                     <h4 class="task-list-empty-title">GG</h4>
+//                     <p class="task-list-empty-description">All your tasks is done!</p>
+//                 </div>`
+//     return(cardElement)
+// })()
 
 const createTaskCardElement = function({id, title, tags, isFinished, ...otherInfo}) {
     //Собираем новую task-карточку
@@ -43,7 +97,7 @@ const createTaskCardElement = function({id, title, tags, isFinished, ...otherInf
                     <div class="task-card finished pixel-corners">
                         <h3 class="task-card-title">${title}</h3>
                         <div class="task-card-buttons">
-                            <button title="Finish the task" type="button" class="task-card-button button  pixel-corners--wrapper">
+                            <button title="Finish the task" type="button" class="task-card-button button-finish button  pixel-corners--wrapper">
                                 <span class="visually-hidden">Finish the task</span>
                                 V
                             </button>
@@ -69,6 +123,13 @@ const createTaskCardElement = function({id, title, tags, isFinished, ...otherInf
         newTagList.insertAdjacentElement("beforeend", tagElement)
     })
 
+    const finishButton = newTaskCard.querySelector('.button-finish')
+    const deleteButton = newTaskCard.querySelector('.button-delete')
+
+    finishButton.onclick = finishTask
+    deleteButton.onclick = deleteTask
+
+    //Проверка, выполнена ли задача
     if (isFinished) {
         newTaskCard.querySelector('.task-card').classList.add('finished')
     }
@@ -78,6 +139,7 @@ const createTaskCardElement = function({id, title, tags, isFinished, ...otherInf
 
     return(newTaskCard)
 }
+
 
 getTagList = function (tasks) {
     const tagList = []
@@ -107,26 +169,79 @@ filterTasks = function(tasks, filterOption){
     }
 }
 
+const finishTask = function(){
+    const taskItem = this.parentElement.parentElement.parentElement
+    const taskCard = this.parentElement.parentElement
+    const taskId = +(taskItem.id?.replace('taskItem-',''))
+    const task = getTaskById(taskId)
+    if (task.isFinished) {
+        task.isFinished = false
+        taskCard.classList.remove('finished')
+    }
+    else {
+        task.isFinished = true
+        taskCard.classList.add('finished')
+    }
+
+    saveTasks(tasks)
+}
+
+const deleteTask = function(){
+    const taskItem = this.parentElement.parentElement.parentElement
+    const taskId = +(taskItem.id?.replace('taskItem-',''))
+    const taskIndex = getTaskIndexById(taskId)
+    isDev&&console.log('deleteTask log:\n', 'taskitem:', taskItem, '\ntaskId:', taskId, '\ntaskIndex:', taskIndex)
+    if (taskIndex != null) {
+        tasks.splice(taskIndex, 1)
+        taskItem.remove()
+        updateListFilter(tasks)
+        saveTasks(tasks)
+    }
+
+}
+
+
+
 const updateListFilter = function(tasks) {
     const tagList = getTagList(tasks)
+    if (!tagList.includes(currentOptionTag)) {
+        currentOptionTag = 'all'
+        renderTaskList(tasks, currentOptionTag)
+    }
+    isDev&&console.log('updateListFilter log:\ntasks: ',tasks, '\ntagList: ', tagList)
+    listFilter.querySelectorAll('option').forEach((option)=>option.remove())
+    listFilter.options[0] = new Option('All', 'all')
     for (let tagIndex = 0; tagIndex < tagList.length; tagIndex++ ){
         listFilter.options[tagIndex+1] = new Option(`${tagList[tagIndex]}`, `${tagList[tagIndex]}`)
-
     }
 }
 
 const renderTaskList = function (tasks, filterOption) { //,filterOption
-    const filteredTasks = filterTasks(tasks, filterOption)
-    console.log(`мы в рендере и фильтр опшна ${filterOption}, а таска отфильтрна`, filteredTasks)
+    const tagList = getTagList(tasks)
 
-    filteredTasks.forEach(({id, title, tags, isFinished, ...otherInfo}) => {
-        // console.log(id, title, tags,isFinished, otherInfo)
-        if (taskList.querySelector(`#taskItem-${id}`) === null) {
-            const taskElement = createTaskCardElement({id, title, tags, isFinished})
-            taskList.insertAdjacentElement('beforeend', taskElement)
-        }
-    })
+    isDev&&console.log(`
+    tasks: ${tasks}
+    filterOption: ${filterOption}
+    currentOptionTag: ${currentOptionTag} 
+    if (hasTag): ${filterOption === 'all' || tagList.includes(filterOption)}
+    `)
 
+    if (filterOption === 'all' || tagList.includes(filterOption)) {
+        emptyCardElement.classList.add('hidden')
+        listFilter.querySelector(`option[value="${filterOption}"]`)?.setAttribute('selected', '')
+        const filteredTasks = filterTasks(tasks, filterOption)
+
+        filteredTasks.forEach(({id, title, tags, isFinished, ...otherInfo}) => {
+
+            if (taskList.querySelector(`#taskItem-${id}`) === null) {
+                const taskElement = createTaskCardElement({id, title, tags, isFinished})
+                taskList.insertAdjacentElement('beforeend', taskElement)
+            }
+        })
+    }
+    else {
+        emptyCardElement.classList.remove('hidden')
+    }
 }
 
 
@@ -136,7 +251,7 @@ const renderTaskList = function (tasks, filterOption) { //,filterOption
 const tagLists = document.querySelectorAll('.task-card-tag-list');
 tagLists.forEach((tagList) => {
     const tagItems = tagList.querySelectorAll('.task-card-tag-item');
-    console.log()
+
     //Если в списке нет элементов
     if (tagList.children.length === 0) {
         tagList.style.top = '25px'
@@ -145,7 +260,7 @@ tagLists.forEach((tagList) => {
 
 
     for (let item = tagList.children.length; item--; item > 0) {
-        console.log(`scrollWidth: ${tagList.scrollWidth-4}, clWidth: ${tagList.clientWidth}`)
+        isDev&&console.log(`scrollWidth: ${tagList.scrollWidth-4}, clWidth: ${tagList.clientWidth}`)
         if (tagList.scrollWidth-4 > tagList.clientWidth) {
             tagList.children.item(item).remove()
         }
@@ -183,13 +298,21 @@ tagLists.forEach((tagList) => {
 
 
 
+
+
 //Слушатель на изменеие фильтра:
 listFilter.addEventListener("change", function (){
-    console.log('Сменились ёпты', this.value)
+    currentOptionTag = this.value
     taskList.innerHTML = ``
     renderTaskList(tasks, this.value)
 })
 //Submit TaskForm
+
+
+
+
+
+
 addEventListener("submit", (event)=>{
     event.preventDefault()
     //Дешифруем строку на теги и тайтл
@@ -208,6 +331,7 @@ addEventListener("submit", (event)=>{
 
     const newTask = new Task(~~(Math.random()*100), taskTitle, taskTags, false)
     tasks.push(newTask)
+    saveTasks(tasks)
 
     renderTaskList(tasks, listFilter.value)
     updateListFilter(tasks)
@@ -215,3 +339,7 @@ addEventListener("submit", (event)=>{
     titleInput.value = ''
     titleInput.focus()
 })
+
+
+updateListFilter(tasks)
+renderTaskList(tasks, 'all')
